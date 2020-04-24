d7ebfae37b3e94b32d2f9297b020b628dfdc9639
Telougat <thelorenzo533@gmail.com>
2020-04-24 11:11:47 +0200

    Fix carrousel bug

--- public/js/carrousel.js
+++ public/js/carrousel.js
@@ -127,124 +127,6 @@ module.exports = function _atob(str) {
 
 /***/ }),
 
-/***/ "./node_modules/bezier-easing/src/index.js":
-/*!*************************************************!*\
-  !*** ./node_modules/bezier-easing/src/index.js ***!
-  \*************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-/**
- * https://github.com/gre/bezier-easing
- * BezierEasing - use bezier curve for transition easing function
- * by Gaëtan Renaudeau 2014 - 2015 – MIT License
- */
-
-// These values are established by empiricism with tests (tradeoff: performance VS precision)
-var NEWTON_ITERATIONS = 4;
-var NEWTON_MIN_SLOPE = 0.001;
-var SUBDIVISION_PRECISION = 0.0000001;
-var SUBDIVISION_MAX_ITERATIONS = 10;
-
-var kSplineTableSize = 11;
-var kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
-
-var float32ArraySupported = typeof Float32Array === 'function';
-
-function A (aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
-function B (aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
-function C (aA1)      { return 3.0 * aA1; }
-
-// Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-function calcBezier (aT, aA1, aA2) { return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT; }
-
-// Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-function getSlope (aT, aA1, aA2) { return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1); }
-
-function binarySubdivide (aX, aA, aB, mX1, mX2) {
-  var currentX, currentT, i = 0;
-  do {
-    currentT = aA + (aB - aA) / 2.0;
-    currentX = calcBezier(currentT, mX1, mX2) - aX;
-    if (currentX > 0.0) {
-      aB = currentT;
-    } else {
-      aA = currentT;
-    }
-  } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
-  return currentT;
-}
-
-function newtonRaphsonIterate (aX, aGuessT, mX1, mX2) {
- for (var i = 0; i < NEWTON_ITERATIONS; ++i) {
-   var currentSlope = getSlope(aGuessT, mX1, mX2);
-   if (currentSlope === 0.0) {
-     return aGuessT;
-   }
-   var currentX = calcBezier(aGuessT, mX1, mX2) - aX;
-   aGuessT -= currentX / currentSlope;
- }
- return aGuessT;
-}
-
-function LinearEasing (x) {
-  return x;
-}
-
-module.exports = function bezier (mX1, mY1, mX2, mY2) {
-  if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1)) {
-    throw new Error('bezier x values must be in [0, 1] range');
-  }
-
-  if (mX1 === mY1 && mX2 === mY2) {
-    return LinearEasing;
-  }
-
-  // Precompute samples table
-  var sampleValues = float32ArraySupported ? new Float32Array(kSplineTableSize) : new Array(kSplineTableSize);
-  for (var i = 0; i < kSplineTableSize; ++i) {
-    sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
-  }
-
-  function getTForX (aX) {
-    var intervalStart = 0.0;
-    var currentSample = 1;
-    var lastSample = kSplineTableSize - 1;
-
-    for (; currentSample !== lastSample && sampleValues[currentSample] <= aX; ++currentSample) {
-      intervalStart += kSampleStepSize;
-    }
-    --currentSample;
-
-    // Interpolate to provide an initial guess for t
-    var dist = (aX - sampleValues[currentSample]) / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
-    var guessForT = intervalStart + dist * kSampleStepSize;
-
-    var initialSlope = getSlope(guessForT, mX1, mX2);
-    if (initialSlope >= NEWTON_MIN_SLOPE) {
-      return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
-    } else if (initialSlope === 0.0) {
-      return guessForT;
-    } else {
-      return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize, mX1, mX2);
-    }
-  }
-
-  return function BezierEasing (x) {
-    // Because JavaScript number are imprecise, we should guarantee the extremes are right.
-    if (x === 0) {
-      return 0;
-    }
-    if (x === 1) {
-      return 1;
-    }
-    return calcBezier(getTForX(x), mY1, mY2);
-  };
-};
-
-
-/***/ }),
-
 /***/ "./node_modules/draw-image-normalized/index.js":
 /*!*****************************************************!*\
   !*** ./node_modules/draw-image-normalized/index.js ***!
@@ -20653,8 +20535,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var gsap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! gsap */ "./node_modules/gsap/index.js");
 /* harmony import */ var rect_crop__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! rect-crop */ "./node_modules/rect-crop/index.js");
 /* harmony import */ var rect_crop__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(rect_crop__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var bezier_easing__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! bezier-easing */ "./node_modules/bezier-easing/src/index.js");
-/* harmony import */ var bezier_easing__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(bezier_easing__WEBPACK_IMPORTED_MODULE_2__);
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 var KenBurns = __webpack_require__(/*! kenburns */ "./node_modules/kenburns/lib/index.js");
@@ -20672,19 +20552,6 @@ var carrouselDiv = document.getElementById('carrousel'); //Select carrousel's ma
 // Ajax function : get JSON data for built the carrousel
 
 $('document').ready(function () {
-  var kenImage = document.getElementById('image');
-  var kenDiv = document.getElementById('div');
-  var kenburn = new KenBurns.DOM(kenDiv);
-  kenburn.animate(kenImage, rect_crop__WEBPACK_IMPORTED_MODULE_1___default()(0.4, [0.2, 0.38]), rect_crop__WEBPACK_IMPORTED_MODULE_1___default.a.largest, 5000, bezier_easing__WEBPACK_IMPORTED_MODULE_2___default()(0.6, 0.0, 1.0, 1.0));
-  var image = document.getElementById('image');
-
-  image.onload = function () {
-    console.log(image);
-    var div = document.getElementById('div');
-    var kenBurns = new KenBurns.DOM(div);
-    console.log(kenBurns);
-    kenBurns.animate(image, rect_crop__WEBPACK_IMPORTED_MODULE_1___default()(0.5, center[(1, 5)]), rect_crop__WEBPACK_IMPORTED_MODULE_1___default.a.largest, 3000, bezier_easing__WEBPACK_IMPORTED_MODULE_2___default()(0.6, 0.0, 1.0, 1.0));
-  };
   carrouselTimeline.pause(); //Stop the timeline
 
   var tl = gsap__WEBPACK_IMPORTED_MODULE_0__["gsap"].timeline(); //Start other timeline animtions (header, etc...)
@@ -20729,7 +20596,7 @@ $('document').ready(function () {
       scale: 1,
       ease: "back"
     }, ">");
-    carrouselTimeline.call(launchKenEffect, [id, data.firstZoom, data.firstX, data.firstY, data.secondZoom, data.secondX, data.secondY, data.duration, document.getElementById('kenImage' + id).naturalWidth, document.getElementById('kenImage' + id).naturalHeight], "<");
+    carrouselTimeline.call(launchKenEffect, [id, data.firstZoom, data.firstX, data.firstY, data.secondZoom, data.secondX, data.secondY, data.duration, document.getElementById('kenImage' + id).naturalWidth, document.getElementById('kenImage' + id).naturalHeight]);
     carrouselTimeline.to("#kenDiv" + id, {
       duration: 1.2,
       x: -2000,
@@ -20753,6 +20620,7 @@ $('document').ready(function () {
           var data = response;
 
           for (var x = 0; x < data.length; x++) {
+            console.log('test');
             addImageToCarrousel(x, data[x].name);
             addImageToTimeline(x, data[x]);
           }
